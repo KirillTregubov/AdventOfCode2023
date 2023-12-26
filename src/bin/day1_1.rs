a7510987f1550c2a5c6a47715720924bfc502007
Kirill Tregubov <contact@kirilltregubov.com>
2023-12-25 20:05:48 -0500

    day 1 part 1 finished

---
+++ src/bin/day1_1.rs
@@ -0,0 +1,28 @@
+use utils::read_input;
+
+fn main() {
+    let file: &str = "day1.txt";
+    let contents = read_input(file);
+
+    let mut sum: i32 = 0;
+
+    for line in contents.lines() {
+        let first_num = line
+            .find(|c: char| c.is_digit(10))
+            .expect("Input is malformed");
+        let last_num = line
+            .rfind(|c: char| c.is_digit(10))
+            .expect("Input is malformed");
+
+        let new_num: i32 = format!(
+            "{}{}",
+            line.chars().nth(first_num).unwrap(),
+            line.chars().nth(last_num).unwrap()
+        )
+        .parse()
+        .expect("Input is malformed");
+        sum += new_num;
+    }
+
+    println!("Solution to Day 1 Part One: {}", sum);
+}
--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,51 @@
-use std::fs;
-
 pub fn read_input(file: &str) -> String {
+    use std::fs;
+
     let path = format!("inputs/{}", file);
     let contents = fs::read_to_string(&path).unwrap_or_else(|err| {
         panic!("Error reading file '{}': {}", path, err);
     });
     return contents;
+}
+
+/// Measures the runtime of the provided code and prints the elapsed time.
+///
+/// # Arguments
+///
+/// * `code_to_profile` - A closure containing the code to be profiled.
+///
+/// # Examples
+///
+/// ```
+/// // Example usage of the profile function
+/// utils::profile(|| {
+///     // Your program logic goes here
+/// });
+/// ```
+///
+/// This function prints the elapsed time in seconds and milliseconds.
+pub fn profile<F>(code_to_profile: F)
+where
+    F: FnOnce(),
+{
+    use std::time::Instant;
+
+    // Record the start time
+    let start_time = Instant::now();
+
+    // Call the provided closure
+    code_to_profile();
+
+    // Record the end time
+    let end_time = Instant::now();
+
+    // Calculate the elapsed time
+    let elapsed_time = end_time - start_time;
+
+    // Print the elapsed time in seconds and milliseconds
+    println!(
+        "Elapsed time: {} seconds, {} milliseconds",
+        elapsed_time.as_secs(),
+        elapsed_time.subsec_millis()
+    );
 }
